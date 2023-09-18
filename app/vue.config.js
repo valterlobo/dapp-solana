b4a849ee0d64809785a33f4b3172b0329135ba03
valterlobo <valterlobo@gmail.com>
2023-09-17 22:42:42 -0300

    feat: start app mock

---
+++ app/vue.config.js
@@ -0,0 +1,30 @@
+const webpack = require('webpack')
+const { defineConfig } = require('@vue/cli-service')
+
+module.exports = defineConfig({
+  transpileDependencies: true,
+  configureWebpack: {
+    plugins: [
+      new webpack.ProvidePlugin({
+        Buffer: ['buffer', 'Buffer']
+      })
+    ],
+    resolve: {
+      fallback: {
+        crypto: false,
+        fs: false,
+        assert: false,
+        process: false,
+        util: false,
+        path: false,
+        stream: false,
+        http: false,
+        https: false,
+        tls: false,
+        net: false,
+        zlib: false,
+        url: require.resolve("url/")
+      }
+    }
+  }
+})
